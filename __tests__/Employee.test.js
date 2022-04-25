75bc74734ebe8825078ea52e7284a68eebd2d8d7
Benjamin Eidum <94485806+Benjamineidum@users.noreply.github.com>
2022-04-25 11:56:47 -0400

    Update Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,34 +3,34 @@ const Employee = require('../lib/Employee.js');
 describe('Employee', () => {
     describe('getName', () => {
         it("should get the name of the employee", () => {
-            const employee = new Employee('Yolanda', 5, 'yolo@gmail.com');
+            const employee = new Employee('Brayden', 5, 'Brayden@Target.com');
             const name = employee.getName();
 
-            expect(name).toBe('Yolanda');
+            expect(name).toBe('Brayden');
         });
     });
 
     describe('getId', () => {
         it("should get the id of the employee", () => {
-            const employee = new Employee('Steve', 2, 'steve@gmail.com');
+            const employee = new Employee('Chief', 117, 'john117@unsc.com');
             const id = employee.getId();
 
-            expect(id).toEqual(2);
+            expect(id).toEqual(117);
         });
     });
 
     describe('getEmail', () => {
         it("should get the email of the employee", () => {
-            const employee = new Employee('Ron', 4, 'CaptainRon@yahoo.com');
+            const employee = new Employee('Kristina', 1, 'Kristina@Target.com');
             const email = employee.getEmail();
 
-            expect(email).toEqual('CaptainRon@yahoo.com');
+            expect(email).toEqual('Kristina@Target.com');
         });
     });
 
     describe('getRole', () => {
         it("should return the role as Employee", () => {
-            const employee = new Employee('Michael', 5, 'michaelscott@dundermifflin.com');
+            const employee = new Employee('Karl', 5, 'KarlAnthonyTowns@timberwolves.com');
             const role = employee.getRole();
 
             expect(role).toEqual('Employee');
