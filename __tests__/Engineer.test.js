ea988fd1aaed30f0baf9d491da46388c0abe9b1a
Benjamin Eidum <94485806+Benjamineidum@users.noreply.github.com>
2022-04-25 12:15:37 -0400

    Update Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,16 +3,16 @@ const Engineer = require('../lib/Engineer');
 describe('Engineer', () => {
     describe('getGithub', () => {
         it('should get the github link', () => {
-            const engineer = new Engineer('Henry', 1, 'henry@gmail.com', 'henry');
+            const engineer = new Engineer('Emily', 1, 'emilybooknburger@gmail.com', 'Emily');
             const github = engineer.getGithub();
 
-            expect(github).toEqual("henry");
+            expect(github).toEqual("Emily");
         });
     });
 
     describe('getRole', () => {
         it('should get the engineer role', () => {
-            const engineer = new Engineer('Henry', 1, 'henry@gmail.com', 'henry');
+            const engineer = new Engineer('Derek', 1, 'Derekeidum@duke.edu', 'Derek');
             const role = engineer.getRole();
 
             expect(role).toEqual("Engineer");
