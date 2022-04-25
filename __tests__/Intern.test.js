52fa169d50300700b61cde842464093d6492cd2b
Benjamin Eidum <94485806+Benjamineidum@users.noreply.github.com>
2022-04-25 12:13:45 -0400

    Update Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,16 +3,16 @@ const Intern = require('../lib/Intern');
 describe('Intern', () => {
     describe('getSchool', () => {
         it('should get the name of the school', () => {
-            const intern = new Intern('Henry', 1, 'henry@gmail.com', 'U of M');
+            const intern = new Intern('Xavier', 1, 'Xavier@gmail.com', 'Georgia State University');
             const school = intern.getSchool();
 
-            expect(school).toEqual("U of M");
+            expect(school).toEqual("Georgia State University");
         });
     });
 
     describe('getRole', () => {
         it('should get the intern role', () => {
-            const intern = new Intern('Henry', 1, 'henry@gmail.com', 'U of M');
+            const intern = new Intern('Billie', 1, 'GoldengirlBillie@gmail.com', 'Puppy State College');
             const role = intern.getRole();
 
             expect(role).toEqual("Intern");
